f47de0d122f8a2e5e93cf076298e22fc099e23da
josephquang97 <=>
2022-01-14 03:20:58 +0000

    reversed part1

--- tests/part1.cpp
+++ tests/part1.cpp
@@ -140,7 +140,6 @@ TEST_CASE("HSLAPixel should have member a as double", "[weight=1]") {
   REQUIRE(uiuc_testing::is_double<decltype(HSLAPixel::a)>::value);
 }
 
-
 // HSLAPixel default constructor
 TEST_CASE("HSLAPixel's default constructor should create a white pixel", "[weight=1]") {
   HSLAPixel pixel;
@@ -151,116 +150,6 @@ TEST_CASE("HSLAPixel's default constructor should create an opaque pixel", "[wei
   HSLAPixel pixel;
   REQUIRE( pixel.a == 1 );
 }
-
-/*
-// HSLAPixel 3-param constructor
-TEST_CASE("HSLAPixel's HSL constructor should create a pixel with provided HSL values", "[weight=1]") {
-  HSLAPixel pixel(260, 0.8, 0.7);
-  REQUIRE( pixel.h == 260 );
-  REQUIRE( pixel.s == 0.8 );
-  REQUIRE( pixel.l == 0.7 );
-}
-
-TEST_CASE("HSLAPixel's HSL constructor should create an opaque pixel", "[weight=1]") {
-  HSLAPixel pixel(260, 0.8, 0.7);
-  REQUIRE( pixel.a == 1 );
-}
-
-
-// HSLAPixel 4-param constructor
-TEST_CASE("HSLAPixel's HSLA constructor should create a pixel with provided HSLA values", "[weight=1]") {
-  HSLAPixel pixel(260, 0.8, 0.7, 0.4);
-  REQUIRE( pixel.h == 260 );
-  REQUIRE( pixel.s == 0.8 );
-  REQUIRE( pixel.l == 0.7 );
-  REQUIRE( pixel.a == 0.4 );
-}
-
-*/
-PNG createRainbowPNG() {
-  PNG png(360, 100);
-  for (unsigned x = 0; x < png.width(); x++) {
-    for (unsigned y = 0; y < png.height(); y++) {
-      HSLAPixel & pixel = png.getPixel(x, y);
-      pixel.h = x;
-      pixel.s = y / 100.0;
-      pixel.l = 0.5;
-    }
-  }
-  return png;
-}
-
-PNG createWatermark() {
-	PNG png(360, 100);
-  for (unsigned x = 0; x < png.width(); x++) {
-    for (unsigned y = 0; y < png.height(); y++) {
-      HSLAPixel & pixel = png.getPixel(x, y);
-      if (y >= 10 && y <= 30) { pixel.l = 1; }
-      else                    { pixel.l = 0; }
-    }
-  }
-  return png;
-}
-
-
-TEST_CASE("illinify", "[weight=1]") {
-  PNG png = createRainbowPNG();
-  PNG result = illinify(png);
-
-  SECTION("After illinify, image should contain only orange and blue hues") {
-    for (unsigned x = 0; x < result.width(); x++) {
-      for (unsigned y = 0; y < result.height(); y++) {
-        if (result.getPixel(x, y).h != 11 && result.getPixel(x, y).h != 216) {
-          FAIL("The hue of (" << x << "," << y <<") is " << result.getPixel(x, y).h << ", not Illini orange or blue.");
-        }
-      }
-    }
-  }
-
-  SECTION("Pixels closest to blue should become blue") {
-    REQUIRE( result.getPixel(200, 4).h == 216 );
-    REQUIRE( result.getPixel(210, 12).h == 216 );
-    REQUIRE( result.getPixel(220, 23).h == 216 );
-    REQUIRE( result.getPixel(230, 44).h == 216 );
-  }
-
-  SECTION("Pixels closest to orange should become orange") {
-    REQUIRE( result.getPixel(10, 4).h == 11 );
-    REQUIRE( result.getPixel(30, 12).h == 11 );
-    REQUIRE( result.getPixel(40, 23).h == 11 );
-    REQUIRE( result.getPixel(40, 44).h == 11 );
-  }
-
-}
-
-TEST_CASE("illinify_edge_case", "[weight=1]") {
-  PNG png = createRainbowPNG();
-  PNG result = illinify(png);
-
-  SECTION("Reassigned hues should wrap around correctly: h=359 is closer to orange (11) than blue (216)") {
-    REQUIRE( result.getPixel(330, 4).h == 11 );
-    REQUIRE( result.getPixel(340, 12).h == 11 );
-    REQUIRE( result.getPixel(350, 23).h == 11 );
-  }
-}
-
-TEST_CASE("watermark", "[weight=1]") {
-  PNG png = createRainbowPNG();
-  PNG wm = createWatermark();
-
-  PNG result = watermark(png, wm);
-
-  SECTION("Pixels without watermark should be unchanged") {
-    REQUIRE( png.getPixel(100, 50).l == result.getPixel(100, 50).l );
-    REQUIRE( png.getPixel(200, 80).l == result.getPixel(200, 80).l );
-  }
-
-  SECTION("Pixels with watermark should be changed") {
-    REQUIRE( png.getPixel(100, 15).l + 0.2 == result.getPixel(100, 15).l );
-    REQUIRE( png.getPixel(200, 25).l + 0.2 == result.getPixel(200, 25).l );
-  }
-}
-
 
 // The following tests are disabled.
 // They evaluate constructors, but the Week 1-2 content doesn't cover constructors currently.
